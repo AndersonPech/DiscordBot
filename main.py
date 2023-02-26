e2cc5bb150696875584a150729be5fbd3ab140b9
Anderson Pech <andersonpech@gmail.com>
2023-02-26 21:52:34 +1100

    f

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import discord
 import logging
+import datetime
+
+'''
+Wish someone a happy birthday via discord
+'''
+
+#MTA3ODY3MTQ0NDE3NjgwNTk1OA.GjjA55.gDgzSqCTVOnrtW077ymuOdlklY7hOjB02OhxBk
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
+
+'''
+Read information from google sheets
+'''
+def ReadSheets():
+    pass
+
+
+'''
+If there is someone with a bday today, wish them Hbday otherwise sleep for 24 hours
+'''
+def BirthdayCheck():
+    pass
+
+
+
+'''
+Discord client
+'''
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
