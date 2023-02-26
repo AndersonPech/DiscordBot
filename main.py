c5b19b0757a31d1c205414365fa81e23b6d7fcb9
Anderson Pech <andersonpech@gmail.com>
2023-02-26 16:13:19 +1100

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import logging
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('my token goes here', log_handler=handler, log_level=logging.DEBUG)
+
